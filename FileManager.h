4260d855a9bff0e96b68bfc731e42eb65c311826
Przemyslaw Tarchala <przemyslaw.tarchala.programista@gmail.com>
2023-09-04 18:27:02 +0200

    Create class FileManager

---
+++ FileManager.h
@@ -0,0 +1,40 @@
+#include "User.h"
+
+void User::setId(int newId){
+    id = newId;
+}
+
+void User::setName(string newName){
+    name = newName;
+}
+
+void User::setLastname(string newLastname){
+    lastname = newLastname;
+}
+
+void User::setLogin(string newLogin){
+    login = newLogin;
+}
+
+void User::setPassword(string newPassword){
+    password = newPassword;
+}
+
+int User::getId(){
+    return id;
+}
+string User::getName(){
+    return name;
+}
+
+string User::getLastname(){
+    return lastname;
+}
+
+string User::getLogin(){
+    return login;
+}
+string User::getPassword(){
+    return password;
+}
+
